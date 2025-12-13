33e70390e4365b9a18bd6d032569cff30c747291
tom <tom.hervieu@gmail.com>
2025-12-13 15:54:06 -0500

    chore(prettier): add prettier and associated tailwind plugin

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import type { PropsWithChildren } from "react";
 
 const AppLayout = (props: PropsWithChildren) => {
   return (
-    <div className="grid grid-rows-[2fr_9fr_1fr] h-screen w-screen px-safe py-safe bg-zinc-50 font-main dark:bg-black">
+    <div className="px-safe py-safe font-main grid h-screen w-screen grid-rows-[2fr_9fr_1fr] bg-zinc-50 dark:bg-black">
       {props.children}
     </div>
   );
 };
 
 const HostContainer = () => {
-  return <div className="row-start-1 row-end-2 bg-night">first</div>;
+  return <div className="bg-night row-start-1 row-end-2">first</div>;
 };
 
 const MainContentContainer = () => {
-  return <div className="row-start-2 row-end-3 bg-other">sec</div>;
+  return <div className="bg-other row-start-2 row-end-3">sec</div>;
 };
 
 const UserPanelContainer = () => {
-  return <div className="row-start-3 row-end-4 bg-dark-green">th</div>;
+  return <div className="bg-dark-green row-start-3 row-end-4">th</div>;
 };
 
 export const App = () => {
