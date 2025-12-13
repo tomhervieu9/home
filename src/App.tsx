b58dae3b04a1eca7195df157e1b04df2b5a3bae5
tom <tom.hervieu@gmail.com>
2025-12-13 14:01:57 -0500

    chore: set safe areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import type { PropsWithChildren } from "react";
 
 const AppLayout = (props: PropsWithChildren) => {
-  return <div>{props.children}</div>;
+  return (
+    <div className="grid grid-rows-[3fr_8fr_1fr] h-screen w-screen px-safe py-safe">
+      {props.children}
+    </div>
+  );
+};
+
+const HostContainer = () => {
+  return <div className="row-start-1 row-end-2">first</div>;
+};
+
+const MainContentContainer = () => {
+  return <div className="row-start-2 row-end-3">sec</div>;
+};
+
+const UserPanelContainer = () => {
+  return <div className="row-start-3 row-end-4">th</div>;
 };
 
 export const App = () => {
-  return <AppLayout>hello there!</AppLayout>;
+  return (
+    <AppLayout>
+      <HostContainer />
+      <MainContentContainer />
+      <UserPanelContainer />
+    </AppLayout>
+  );
 };
