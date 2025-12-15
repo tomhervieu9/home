c12283f0b295fa812b4623131468e8e7a240cc6b
Thomas <tom.hervieu@gmail.com>
2025-12-14 19:48:36 -0500

    fix(host): resolve image path correctly

--- src/components/host/Host.tsx
+++ src/components/host/Host.tsx
@@ -1,7 +1,8 @@
+import hostImage from "@home/assets/portfolio-shot.png";
 import { DynamicImage } from "@home/components/shared/DynamicImage";
 
 export const Host = () => {
   return (
-    <DynamicImage src="/images/portfolio-shot.png" alt="Profile Picture" />
+    <DynamicImage src={hostImage} alt="Profile Picture" />
   );
 };
