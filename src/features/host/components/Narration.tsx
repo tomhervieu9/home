d44ed698de337069b5027970226bbaab64255b49
Thomas <tom.hervieu@gmail.com>
2025-12-28 18:31:29 -0500

    chore(host): add narration text state

--- src/features/host/components/Narration.tsx
+++ src/features/host/components/Narration.tsx
@@ -1,7 +1,14 @@
 import { cn } from "@home/utils/css/classNames";
+import { useState } from "react";
 
 export const Narration = (props: { className?: string }) => {
+  const [text, setText] = useState(
+    "Welcome to my internet home! \n \n To get started, please scroll through the main content and refer to this panel for additional details.",
+  );
+
   return (
-    <div className={cn(props.className, "wrap-break-word")}>narration...</div>
+    <div className={cn(props.className, "wrap-break-word whitespace-pre-line")}>
+      {text}
+    </div>
   );
 };
