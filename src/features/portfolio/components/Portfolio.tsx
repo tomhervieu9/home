ba318011d4de97f8d3621cc6445e2f1696e16046
Thomas <tom.hervieu@gmail.com>
2025-12-30 01:12:32 -0500

    chore(portfolio): add scrollable divs

--- src/features/portfolio/components/Portfolio.tsx
+++ src/features/portfolio/components/Portfolio.tsx
@@ -5,7 +5,10 @@ export const Portfolio = (props: { className: string }) => {
   return (
     <div className={cn(props.className, "relative")}>
       <Panel className="absolute inset-0 -z-10" />
-      <div className="p-4">portfolio</div>
+      <div className="scrollbar-none wide:m-[.5vw] tall:m-[.35vh] size-full overflow-auto">
+        <div className="size-full bg-amber-600">d</div>
+        <div className="size-full bg-emerald-500">s</div>
+      </div>
     </div>
   );
 };
